ea903f92fdb208f914ba8a2468d0ef474d60bab4
Caysoft <caysoft.dev@gmail.com>
2022-09-23 19:18:58 +0200

    feat(constants): add game constants for powers, spell-schools and unit-flags

---
+++ src/lib/constants/power.ts
@@ -0,0 +1,13 @@
+export enum Power {
+  Health = 0,
+  Mana = 2,
+  Rage = 3,
+  Focus = 4,
+  Energy = 5,
+  Runes = 7,
+  RunicPower = 8,
+  SoulShards = 9,
+  LunarPower = 10,
+  HolyPower = 11,
+  Alternate = 12,
+}
